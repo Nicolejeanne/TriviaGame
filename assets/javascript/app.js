d24ccb81c22d007ec179605b28e2bbe000ca2359
NicJM <nicolejeanne@users.noreply.github.com>
2018-08-30 22:10:50 -0500

    added content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,11 @@
 $(document).ready(function(e){
 
-// function to switch between screen views
-    function showView(viewName) {
-        $('.view').hide();
-        $('#' + viewName).show();
-    }
-    $('[data-launch-view]').click(function (e) {
-        e.preventDefault();
-        var viewName = $(this).attr('data-launch-view');
-        showView(viewName);
-    });
-// end of screen view function
-
 // variables
 let userPick;
 let correctAnswer = 0;
 let wrongAnswer = 0;
 let question = 0;
-let count = 0;
+let count = 10;
 
 let movieQuestion = [{
     question: "Which of the following is NOT one of Derek Zoolander's signature looks?",
@@ -33,39 +21,73 @@ let movieQuestion = [{
     validAnswer: 0
 }];
 
+// function to switch between screen views
+function showView(viewName) {
+    $('.view').hide();
+    $('#' + viewName).show();
+}
+$('[data-launch-view]').click(function (e) {
+    e.preventDefault();
+    var viewName = $(this).attr('data-launch-view');
+    showView(viewName);
+});
+// end of screen view function
+
 // Initializing the game
 function initializeGame(){
-    $(".btn").click(function(){
-        $(this).hide();
-        counter = setInterval(timer, 12000); 
-        displayTrivia();
-        }); 
 
-        function timer(){
-            count--;
-            if (count <= 0) {
-             clearInterval(counter);
-             return;
-            }
-            $(".card-body").html("Time remaining: " + "00:" + count + " secs");
-        }
+//When "Let's Play" button is clicked
+$(".btn-primary").click(function(){
+    
+//Set timer to time remaining, count down per second
+counter = setInterval(timer, 1000);
 
-        function displayTrivia() {
-            $(".card-header").html(movieQuestion[0].question);
-            question++;
-            
-              var choicesArr = movieQuestion[0].choices;
-              var buttonsArr = [];
-            
-              for (let i = 0; i < choicesArr.length; i++) {
-                var button = $('<radio>');
-                button.text(choicesArr[i]);
-                button.attr('data-id', i);
-                $('.defaultInline1').append(button);
-               }
-            
-              } 
+//Display all question and answer options 
+displayTrivia();
+});
 }
+//Timer function
+function timer(){
+    count--;
+    if (count <= 0) {
+        clearInterval(counter);
+        $(".timer").html("Time's up!");
+    }
+    $(".timer").html("Time remaining: " + count);
+} 
+    
+//Display questions function
+function displayTrivia() {
+    $(".card-header").html(movieQuestion[0].question);
+    question++;
+
+    let choicesArr = movieQuestion[0].choices;
+
+    for (let i = 0; i < choicesArr.length; i++) {
+        let radioButton = $('<radio>');
+        radioButton.text(choicesArr[i]);
+        radioButton.attr('data-id', i);
+        $(".label1").append(radioButton);
+        
+    }
+
+} 
+// Choosing an answer   
+$('.blockquote').on('click', 'radio', function(e){
+    userPick = $(this).data("id");
+    movieQuestion[0].validAnswer;
+    if(userPick !== movieQuestion[0].validAnswer) {
+   
+    $('.blockquote').text("Wrong Answer! The correct answer is" + movieQuestion[0].validAnswer);
+    incorrectAnswer++;
+   
+   } else if (userPick === movieQuestion[0].validAnswer) {
+   $('.blockquote').text("Correct!!! The pet tiger name is" + movieQuestion[0].validAnswer);
+   correctAnswer++;
+   
+   }
+});
 
 initializeGame();
+
 });
