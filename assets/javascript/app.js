78130cce079f6487b3c5510ad2ca025a2a7b8e08
NicJM <nicolejeanne@users.noreply.github.com>
2018-09-01 14:52:06 -0500

    working on content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(e){
-
+   
 // variables
 let userPick;
 let correctAnswer = 0;
@@ -11,43 +11,31 @@ let counter;
 let movieQuestion = [{
     question: "Which of the following is NOT one of Derek Zoolander's signature looks?",
     choices: ["Magnum", "Blue Steel", "Kryptonite"],
-    image: ["http://images1.fanpop.com/images/quiz/8413_1213279136018_441_300.jpg"],
+    image: ["../TriviaGame/assets/images/zoolander.jpg"],
     validAnswer: 2
 }, {
     question: "What is the name of Moana's pet pig in her namesake movie?",
     choices: ["Pua", "Hei Hei", "Te Fiti"],
+    image: ["../TriviaGame/assets/images/moana.jpg"],
     validAnswer: 0
 }, {
     question: "Which Jane Austen book is the movie <i>Clueless</i> based on?",
     choices: ["Emma", "Persuasion", "Pride and Prejudice"],
+    image: ["../TriviaGame/assets/images/clueless.jpg"],
     validAnswer: 0
 }];
 
-// function to switch between screen views
-/*function showView(viewName) {
-    $('.view').hide();
-    $('#' + viewName).show();
-}
-$('[data-launch-view]').click(function (e) {
-    e.preventDefault();
-    var viewName = $(this).attr('data-launch-view');
-    showView(viewName);
-});*/
-// end of screen view function
 
 // Initializing the game
 function initializeGame(){
 
-//When "Let's Play" button is clicked
-$(".btn-primary").click(function(){
-    
 //Set timer to time remaining, count down per second
-
 
 //Display all question and answer options 
 displayTrivia();
-});
+
 }
+
 //Timer function
 function timer(){
     count--;
@@ -56,23 +44,24 @@ function timer(){
     if (count <= 0) {
         clearInterval(counter);
         $(".timer").html("Time's up!");
-    }
-    
+    } 
 } 
-    
+
 //Display questions function
 function displayTrivia() {
     count = 10;
     counter = setInterval(timer, 1000);
     $(".card-img-top").attr("src", movieQuestion[index].image);
     $(".card-header").html(movieQuestion[index].question);
-    $(".label1").html(movieQuestion[index].choices[0]);
-    $(".label2").html(movieQuestion[index].choices[1]);
-    $(".label3").html(movieQuestion[index].choices[2]);
-
+    $(".button1").html(movieQuestion[index].choices[0]);
+    $(".button2").html(movieQuestion[index].choices[1]);
+    $(".button3").html(movieQuestion[index].choices[2]);
     index++;
 } 
-document.onkeyup = displayTrivia;
+
+$("button").click(function(){
+    displayTrivia();
+});
 
 // Choosing an answer   
 $('.blockquote').on('click', 'radio', function(e){
